4258af10b7081cd2eaa38127e549d78cef539bcb
chekirou <chekirouhakim@gmail.com>
2019-03-21 15:19:43 +0100

    v3.0
    third dimensions ===============> done 
    backwards aStar ===============> waiting

--- pySpriteWorld-forStudents/DiscreteWorld-coopPathFinding_advanced.py
+++ pySpriteWorld-forStudents/DiscreteWorld-coopPathFinding_advanced.py
@@ -24,17 +24,24 @@ def distManhattan(p1,p2):
     (x1,y1)=p1
     (x2,y2)=p2
     return abs(x1-x2)+abs(y1-y2)
+    
+    
 
-class Noeud:
-    def __init__(self, x,y,  g, pere=None):
+
+def Backwards_search():
+    
+
+class spaceTime_Noeud:
+    def __init__(self, x,y,t,  g, pere=None):
         self.x = x
-        self.y = y 
+        self.y = y
+        self.t = t 
         self.g = g
         self.pere = pere
         
     def __str__(self):
         #return np.array_str(self.etat) + "valeur=" + str(self.g)
-        return str(self.x)+", " +str(self.y)  + " valeur=" + str(self.g)
+        return str(self.x)+", " +str(self.y)  +", " +str(self.t)+ " valeur=" + str(self.g)
         
     def __eq__(self, other):
         return str(self) == str(other)
@@ -48,9 +55,9 @@ class Noeud:
             """
         
         nouveaux_fils = []
-        for s in [(1,0), (0,1), (0,-1), (-1,0)]:
+        for s in [(1,0), (0,1), (0,-1), (-1,0), (0,0)]:
             if (self.x + s[0],self.y+ s[1]) not in wallStates and self.x + s[0]>=0 and self.x + s[0]<=19 and self.y+ s[1]>=0 and self.y+ s[1]<=19:
-                nouveaux_fils.append(Noeud(self.x + s[0],self.y+ s[1], self.g+1,self))
+                nouveaux_fils.append(spaceTime_Noeud(self.x + s[0],self.y+ s[1], self.t+1, self.g+1,self))
         return nouveaux_fils
     def expandNext(self,p,k):
         """ étend un noeud unique, le k-ième fils du noeud n
@@ -77,10 +84,10 @@ class Noeud:
 
 
 
-def astar(initState, goalState, wallStates, obstacles =[]):
+def spaceTime_astar(initState, goalState, wallStates, time , reservations=[],):
     """ application de l'algorithme a-star sur un probleme donné
         """
-    nodeInit = Noeud(initState[0], initState[1],0,None)
+    nodeInit = spaceTime_Noeud(initState[0], initState[1],time, 0,None)
     frontiere = [(nodeInit.g+distManhattan((nodeInit.x, nodeInit.y),goalState),nodeInit)] 
     reserve = {}        
     bestNoeud = nodeInit
@@ -91,19 +98,19 @@ def astar(initState, goalState, wallStates, obstacles =[]):
     # Hypothèse de consistence de l'heuristique
     # ne gère pas les duplicatas dans la frontière
     
-        if str(bestNoeud.x) + " " + str(bestNoeud.y) not in reserve:            
-            reserve[str(bestNoeud.x) + " " + str(bestNoeud.y)] = bestNoeud.g #maj de reserve
+        if str(bestNoeud.x) + " " + str(bestNoeud.y) + " " + str(bestNoeud.t) not in reserve:            
+            reserve[str(bestNoeud.x) + " " + str(bestNoeud.y) + " " + str(bestNoeud.t)] = bestNoeud.g #maj de reserve
             nouveauxNoeuds = bestNoeud.expand(wallStates)            
             for n in nouveauxNoeuds:
-                if (n.x,n.y) not in wallStates and n.x>=0 and n.x<=19 and n.y>=0 and n.y<=19 and (n.x, n.y) not in obstacles:
+                if (n.x,n.y) not in wallStates and n.x>=0 and n.x<=19 and n.y>=0 and n.y<=19 and (n.x, n.y, n.t) not in reservations:
                     f = n.g+distManhattan((n.x, n.y),goalState)
                     heapq.heappush(frontiere, (f,n))       
     # Afficher le résultat  
     res = []
     while(bestNoeud.pere != None):
-        res.append((bestNoeud.x , bestNoeud.y ))   
+        res.append((bestNoeud.x , bestNoeud.y, bestNoeud.t ))   
         bestNoeud = bestNoeud.pere   
-    reversed(res)  
+    res= list(reversed(res))  
        
     
     return res
@@ -115,9 +122,10 @@ def astar(initState, goalState, wallStates, obstacles =[]):
 
 
 
-def occupied(position ,posPlayers):
+def occupied(j, position ,posPlayers):
 	for i in range(len(posPlayers)):
-		if position == posPlayers[i]:
+	
+		if  i != j and position == posPlayers[i]:
 			return (True, i)
 	return (False, -1)
 
@@ -133,36 +141,6 @@ def cross_each_other(posPlayers,wallStates, other_path,actual_path,other, actual
             return True
     else:
         return False
-
-
-def Base_strategy1(nbPlayers, posPlayers, initStates,  wallStates, goalStates):
-    #calculer le chemin sans croiser les autres
-
-    paths = [ [] ]* nbPlayers
-    #etablir les chemins 
-    for joueur in range(nbPlayers):
-        others_goals = [ (goalStates[k] if k!= joueur else None )for k in range(nbPlayers) ]
-        others_initialStates = [ (initStates[k] if k!= joueur else None )for k in range(nbPlayers) ]
-        paths[joueur] = astar(initStates[joueur], goalStates[joueur], wallStates, others_initialStates+others_goals)
-    Lots = [[]]
-    for joueur, path in zip(range(nbPlayers), paths):
-        classé = False
-        index_lots = 0
-        while not classé and index_lots < len(Lots):
-            paralelle = True
-            index_path = 0
-            while paralelle and index_path < len(Lots[index_lots]):
-                if cross_each_other(posPlayers,wallStates, Lots[index_lots][index_path][1], path, Lots[index_lots][index_path][0], joueur):
-                    paralelle = False
-                index_path +=1
-            if paralelle:
-                Lots[index_lots].append((joueur, path))
-                classé = True
-            index_lots += 1
-        if not classé:
-            Lots.append([(joueur, path)])
-    return Lots
-
 
 
 #===================================================================================================================================== 
@@ -188,7 +166,7 @@ game = Game()
 def init(_boardname=None):
     global player,game
     # pathfindingWorld_MultiPlayer
-    name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer1'
+    name = _boardname if _boardname is not None else 'pathfindingWorld_MultiPlayer2'
     game = Game('Cartes/' + name + '.json', SpriteBuilder)
     game.O = Ontology(True, 'SpriteSheet-32x32/tiny_spritesheet_ontology.csv')
     game.populate_sprite_names(game.O)
@@ -236,61 +214,57 @@ def main():
     wallStates = [w.get_rowcol() for w in game.layers['obstacle']]
     #print ("Wallstates:", wallStates)
     posPlayers = initStates
-    
-   
+    #res = [[] for i in range(nbPlayers)]
+    res= []
+    #obj = astar([initStates[0]], [goalStates[0]], wallStates, 0 )
     found = [ False for i in range(nbPlayers)]
-    Lots  = Base_strategy1(nbPlayers,posPlayers, initStates,  wallStates, goalStates)
-    executions = [[] for i in range(nbPlayers)]
-    for lot in Lots:
-        for i in range(iterations):
-            for joueur_path in lot: # on fait bouger chaque joueur séquentiellement
-                j, path = joueur_path
-                if not found[j]:
-                    #row,col = posPlayers[j]
-
-                    x_inc,y_inc = path.pop()
-                    next_row = x_inc
-                    next_col = y_inc
-                    #print(j,"    ",(next_row, next_col))
-                    #print(path)
-                    occu, s = occupied((next_row, next_col) ,posPlayers)
-                    #print("=====================================")
-                    
-                    executions[j].append((next_row,next_col))
-                    players[j].set_rowcol(next_row,next_col)
-                    col=next_col
-                    row=next_row
-                    game.mainiteration()
-                    posPlayers[j]=(row,col)
-                    # si on a  trouvé un objet on le ramasse
-                    if (row,col) in goalStates and goalStates.index((row, col)) == j:
-                        o = players[j].ramasse(game.layers)
-                        print ("Objet trouvé par le joueur ", j)
-                        #goalStates.remove((row,col)) # on enlève ce goalState de la liste
-                        #indice = goalStates.index((row, col))
-                        score[j]+=1
-                        game.mainiteration()
-                        # et on remet un même objet à un autre endroit
-                        #x = random.randint(1,19)
-                        #y = random.randint(1,19)
-                        #while (x,y) in wallStates:
-                        #    x = random.randint(1,19)
-                        #    y = random.randint(1,19)
-                        #o.set_rowcol(x,y)
-                        #goalStates.append((x,y)) # on ajoute ce nouveau goalState
-                        #goalStates[indice] = (x,y) # on ajoute ce nouveau goalState
-                        #initStates[j] = posPlayers[j]
-                        #res[j] = astar(initStates[j], goalStates[j], wallStates )
-                        #game.layers['ramassable'].add(o)
-                        game.mainiteration()                
-                        found[j] = True
-                        break
-    print ("scores:", score)
+    reservations = []
+    for i in range(nbPlayers):
+        print(i)
+        others_goals = [ (goalStates[k] if k!= i else None )for k in range(nbPlayers) ]
+        others_initialStates = [ (initStates[k] if k!= i else None )for k in range(nbPlayers) ]
+        res.append(spaceTime_astar(initStates[i], goalStates[i], wallStates +others_goals+ others_initialStates, 0, reservations ))
+        
+        reservations = reservations +[ (n[0], n[1], n[2]-1) for n in res[i] ] + [ (n[0], n[1], n[2]+1) for n in res[i] ] + res[i]
+        print("joueur " , i, res[i])
+        print("===================================================================")
+        #print("reservations", reservations)
     
+    
+    for i in range(1, iterations):
+        for j in range(nbPlayers): # on fait bouger chaque joueur séquentiellement
+            if not found[j]:
+                next_row, next_col, t = res[j].pop(0)
+                c, k = occupied(j, (next_row, next_col) ,posPlayers)
+                if(c):
+                    print("iteration ", i )
+                    print("je suis , " , j, "a ", next_row, next_col, " croisement avec " , k, t)
+                #if ((next_row,next_col) not in wallStates) and next_row>=0 and next_row<=19 and next_col>=0 and next_col<=19:
+                players[j].set_rowcol(next_row,next_col)
+                col=next_col
+                row=next_row
+                game.mainiteration()
+                posPlayers[j]=(row,col)
+                if (row,col) in goalStates and goalStates.index((row, col)) == j:
+                    o = players[j].ramasse(game.layers)
+                    print ("Objet trouvé par le joueur ", j)
+                    score[j]+=1
+                    game.mainiteration()              
+                    found[j] = True
+                    
+    print("scores:", score)
     pygame.quit()
+    '''
+    t = 1
+    last_times = [res[i][-1][2] for n in range(nbPlayers)]
+    niter = max(last_times)
+    while(t <= niter):
+        moves=[]
+        for i in range(nbPlayers):
+            if(res[i] != [] and t == res[j][0][2]):
+                next_row, next_col, t = res[j].pop(0)
+                moves.append(res[j].pop(0))
+    '''
 
 if __name__ == '__main__':
     main()
-    
-
-
